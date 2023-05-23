5e512dbc746decc70fe1129e67822546b166a1d5
ranawaqarahmad <ranawaqarahmad33@gmail.com>
2023-05-23 17:41:39 +0500

    updated env

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -6,27 +6,22 @@ import { getFirestore } from "@firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+const {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_MESSAGING,
+  VITE_APP_ID,
+} = import.meta.env;
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyDnLwAkFPcZeycjH9j0oTXsfY6ICYITNGo",
-  // authDomain: "hissa-72c27.firebaseapp.com",
-  // projectId: "hissa-72c27",
-  // storageBucket: "hissa-72c27.appspot.com",
-  // messagingSenderId: "586424237388",
-  // appId: "1:586424237388:web:17f3e10711270f095ce3e9",
-  // apiKey: "AIzaSyAug3H3tYIDsimALHfHE7HOyFishrQzIAk",
-  // authDomain: "cow-hissa.firebaseapp.com",
-  // databaseURL: "https://cow-hissa-default-rtdb.firebaseio.com",
-  // projectId: "cow-hissa",
-  // storageBucket: "cow-hissa.appspot.com",
-  // messagingSenderId: "334415757006",
-  // appId: "1:334415757006:web:0d5ea452e878994baa1a9f",
-  apiKey: "AIzaSyA-M3YwurwAG0pfIL_MG9I0lX6MMGiLFFI",
-  authDomain: "eid-ul-adha-706e9.firebaseapp.com",
-  projectId: "eid-ul-adha-706e9",
-  storageBucket: "eid-ul-adha-706e9.appspot.com",
-  messagingSenderId: "105706676705",
-  appId: "1:105706676705:web:8c4157cd28d5c3efc9f2a0"
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGING,
+  appId: VITE_APP_ID,
 };
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
