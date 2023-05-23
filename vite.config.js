6bb7ed6d29b63eb9e5210cfc71d39150a1927288
ranawaqarahmad <ranawaqarahmad33@gmail.com>
2023-05-23 17:41:39 +0500

    updated env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+});
+
+// import { defineConfig, loadEnv } from 'vite'
+
+// export default defineConfig(({ command, mode }) => {
+//   // Load env file based on `mode` in the current working directory.
+//   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+//   const env = loadEnv(mode, process.cwd(), '')
+//   console.log(env.APP_ENV)
+//   return {
+//     // vite config
+//     define: {
+//       __APP_ENV__: env.APP_ENV,
+//     },
+//   }
+// })
